05c883a05853d2894906e738766fa185ac6520e4
WillDavid <wdma.lic17@uea.edu.br>
2020-09-15 10:44:05 -0400

    Add InputScript && PlayerControllerScript

---
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer2D.Character;
+
+[RequireComponent(typeof(CharacterMovement2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerController : MonoBehaviour
+{
+    CharacterMovement2D playerMovement;
+    SpriteRenderer spriteRenderer;
+    PlayerInput playerInput;
+
+
+    public Sprite crouchSprite;
+    public Sprite idleSprite;
+    void Start()
+    {
+        playerMovement = GetComponent<CharacterMovement2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerInput = GetComponent<PlayerInput>();
+        
+        
+    }
+
+    void Update()
+    {
+       Vector2 movementInput = playerInput.GetMovementInput();
+
+        playerMovement.ProcessMovementInput(movementInput);
+        
+
+
+        if(movementInput.x > 0){
+            spriteRenderer.flipX = false;
+        }else if(movementInput.x < 0){
+            spriteRenderer.flipX = true;
+        }else{
+
+        }
+
+
+        if(playerInput.IsJumpButtonDown()){
+            playerMovement.Jump();
+        }
+
+        if(playerInput.IsJumpButtonHeld() == false){
+            //playerMovement.UpdateAbortJump();
+        }
+
+        // Agachar
+
+        if(playerInput.IsCrouchButtonDown()){
+            playerMovement.Crouch();
+            spriteRenderer.sprite = crouchSprite;
+        }else if(playerInput.IsCrouchButtonUp()){
+            playerMovement.UnCrouch();
+            spriteRenderer.sprite = idleSprite;
+        }
+
+
+
+
+
+        
+    }
+}
